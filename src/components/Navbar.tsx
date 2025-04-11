d128c5cf1409d6be31d1ffd76946c95607c578bb
Dibyansu Mohanty <dibyansu.mohanty_pgp2025_25138@mastersunion.org>
2025-04-11 12:07:24 +0530

    Replace text logo with kathalogo.png

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
-            <a href="/" className="text-xl font-bold text-white">Katha: Manga and Manhwa for India</a>
+            <a href="/" className="flex items-center">
+              <img src="/kathalogo.png" alt="Katha Logo" className="h-8 w-auto" />
+            </a>
           </div>
 
           {/* Desktop Navigation */}
